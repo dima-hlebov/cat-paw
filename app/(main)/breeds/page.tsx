62d464a1d8f35aa59c598171034a1e45a4733832
Dmytro <glebov.dymitriy@gmail.com>
2023-09-15 15:08:08 +0300

    fix: responsive

--- app/(main)/breeds/page.tsx
+++ app/(main)/breeds/page.tsx
@@ -15,31 +15,33 @@ export default function Breeds() {
                     <SortForm />
                 </div>
             </div>
-            <Gallery
-                images={[
-                    { id: 1, src: CatPic, alt: "cat" },
-                    { id: 2, src: CatPic, alt: "cat" },
-                    { id: 3, src: CatPic, alt: "cat" },
-                    { id: 4, src: CatPic, alt: "cat" },
-                    { id: 5, src: CatPic, alt: "cat" },
-                    { id: 6, src: CatPic, alt: "cat" },
-                    { id: 7, src: CatPic, alt: "cat" },
-                    { id: 8, src: CatPic, alt: "cat" },
-                    { id: 9, src: CatPic, alt: "cat" },
-                    { id: 10, src: CatPic, alt: "cat" },
-                    { id: 11, src: CatPic, alt: "cat" },
-                    { id: 12, src: CatPic, alt: "cat" },
-                    { id: 13, src: CatPic, alt: "cat" },
-                    { id: 14, src: CatPic, alt: "cat" },
-                    { id: 15, src: CatPic, alt: "cat" },
-                    { id: 16, src: CatPic, alt: "cat" },
-                    { id: 17, src: CatPic, alt: "cat" },
-                    { id: 18, src: CatPic, alt: "cat" },
-                    { id: 19, src: CatPic, alt: "cat" },
-                    { id: 20, src: CatPic, alt: "cat" },
-                    { id: 21, src: CatPic, alt: "cat" },
-                ]}
-                type={GalleryType.LINKS} />
+            <div className="mt-sm sm:mt-md">
+                <Gallery
+                    images={[
+                        { id: 1, src: CatPic, alt: "cat" },
+                        { id: 2, src: CatPic, alt: "cat" },
+                        { id: 3, src: CatPic, alt: "cat" },
+                        { id: 4, src: CatPic, alt: "cat" },
+                        { id: 5, src: CatPic, alt: "cat" },
+                        { id: 6, src: CatPic, alt: "cat" },
+                        { id: 7, src: CatPic, alt: "cat" },
+                        { id: 8, src: CatPic, alt: "cat" },
+                        { id: 9, src: CatPic, alt: "cat" },
+                        { id: 10, src: CatPic, alt: "cat" },
+                        { id: 11, src: CatPic, alt: "cat" },
+                        { id: 12, src: CatPic, alt: "cat" },
+                        { id: 13, src: CatPic, alt: "cat" },
+                        { id: 14, src: CatPic, alt: "cat" },
+                        { id: 15, src: CatPic, alt: "cat" },
+                        { id: 16, src: CatPic, alt: "cat" },
+                        { id: 17, src: CatPic, alt: "cat" },
+                        { id: 18, src: CatPic, alt: "cat" },
+                        { id: 19, src: CatPic, alt: "cat" },
+                        { id: 20, src: CatPic, alt: "cat" },
+                        { id: 21, src: CatPic, alt: "cat" },
+                    ]}
+                    type={GalleryType.LINKS} />
+            </div>
         </div>
     )
 }
