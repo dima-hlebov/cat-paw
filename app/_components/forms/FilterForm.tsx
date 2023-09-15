2dca756200eeb9cb4635a24a91f5bf19a4fcf941
Dmytro <glebov.dymitriy@gmail.com>
2023-09-15 11:58:05 +0300

    fix: change icon

--- app/_components/forms/FilterForm.tsx
+++ app/_components/forms/FilterForm.tsx
@@ -1,15 +1,26 @@
+"use client"
+
 import React from 'react'
+import { Select } from '@components/inputs'
+import IconWrapper, { UpdateIcon, UploadIcon } from '@components/icons'
+import Button from '@components/buttons'
 
 export function FilterForm() {
     return (
         <form
             onSubmit={(e) => e.preventDefault()}
-            className="grid grid-cols-2"
+            className="grid grid-cols-2 gap-x-lg gap-y-sm"
         >
-            <div><select name="" id=""></select></div>
-            <div><select name="" id=""></select></div>
-            <div><select name="" id=""></select></div>
-            <div><select name="" id=""></select></div>
+            <div><Select name='Order' options={["Random"]} /></div>
+            <div><Select name='Type' options={["Static"]} /></div>
+            <div><Select name='Breed' options={["None"]} /></div>
+            <div>
+                <Select name='Limit' options={["Limit"]}>
+                    <Button size={"sm"}>
+                        <IconWrapper Icon={UpdateIcon}></IconWrapper>
+                    </Button>
+                </Select>
+            </div>
         </form>
     )
 }
