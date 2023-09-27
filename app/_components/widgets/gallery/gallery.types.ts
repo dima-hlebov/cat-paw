3b314a6dc4af9a2487c2abe187afdff4914069ab
Dmytro <glebov.dymitriy@gmail.com>
2023-09-27 10:04:03 +0300

    fix: gallery item height

--- app/_components/widgets/gallery/gallery.types.ts
+++ app/_components/widgets/gallery/gallery.types.ts
@@ -3,9 +3,9 @@ import { ImageProps } from "next/image";
 import { LinkProps } from "next/link";
 
 export enum ItemLayout {
-    COLS_1_ROWS_2 = "sm:aspect-[2/3] sm:row-span-2",
-    COLS_2_ROWS_2 = "sm:aspect-[7/5] sm:col-span-2 sm:row-span-2",
-    COLS_1_ROWS_1 = "sm:aspect-[10/7]"
+    COLS_1_ROWS_2 = "h-full sm:aspect-[2/3] sm:row-span-2",
+    COLS_2_ROWS_2 = "h-full sm:aspect-[7/5] sm:col-span-2 sm:row-span-2",
+    COLS_1_ROWS_1 = "h-full sm:aspect-[10/7]"
 }
 
 export type GalleryItemProps = {
