ac1c2a72cafca584101cd3b3ab71efa857f6495a
Dmytro <glebov.dymitriy@gmail.com>
2023-09-29 16:35:43 +0300

    feat: generate user id on landing

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,21 @@
 "use client"
 import ReduxProvider from "@context/Provider";
 import { ThemeProvider } from "next-themes";
+import { randomBytes } from "crypto";
+import { useEffect } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+    // Set user id
+    useEffect(() => {
+        if (!localStorage.getItem("id")) {
+            const id: string = randomBytes(Math.ceil(16 / 2))
+                .toString('hex')
+                .slice(0, 16);
+
+            localStorage.setItem("id", id)
+        }
+    }, [])
+
     return (
         <ReduxProvider>
             <ThemeProvider attribute="class" defaultTheme="light">
